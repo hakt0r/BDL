1e2edee8019a50cbd0d826b44f37ea1fca590bf3
Sebastian Glaser <anx@noreply.github.com>
2021-05-20 13:28:56 +0200

    las fixes

--- verify.sh
+++ verify.sh
@@ -1,8 +1,11 @@
- for i in Grzegorz-Gebala Malika-1989 belaLababidi DCIProject Hamiddci Eko450-eng stasiks1001 BensDevDe midoritiba belowsurface3000 teejayblack Andreydci Sempris ramazankarisan AntonisZaravelas Spearsofcodes naomininnig noureddinalsabbagh; do
+echo repo-ex
+
+for i in Grzegorz-Gebala Malika-1989 belaLababidi DCIProject Hamiddci Eko450-eng stasiks1001 BensDevDe midoritiba belowsurface3000 teejayblack Andreydci Sempris ramazankarisan AntonisZaravelas Spearsofcodes naomininnig noureddinalsabbagh; do
   curl https://github.com/$i/repo-ex 2>/dev/null | grep main.README > /dev/null && echo $i ok || echo $i fail https://github.com/$i/repo-ex
 done;
 
 # https://github.com/FbW-E05-2/everything-begins-hakt0r
+echo everything-begins
 
 for i in Grzegorz-Gebala Malika-1989 belaLababidi \
   DCIProject Hamiddci Eko450-eng stasiks1001 BensDevDe midoritiba\
