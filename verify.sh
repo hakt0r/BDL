dcb00959db4fbd1f003ba5917cdc52c921126aff
Eko450-eng <ekrem@wipdesign.de>
2021-05-20 15:55:48 +0200

    fixed your verify bug thingy

--- verify.sh
+++ verify.sh
@@ -12,7 +12,7 @@ for i in Grzegorz-Gebala Malika-1989 belaLababidi \
     grep -q 200 && echo $i ok || echo $i fail https://raw.githubusercontent.com/FbW-E05-2/everything-begins-$i/main/bar
 done
 
-for i in hakt0r Grzegorz-Gebala Malika-1989 belaLababidi \
+for i in Grzegorz-Gebala Malika-1989 belaLababidi \
   DCIProject Hamiddci Eko450-eng stasiks1001 BensDevDe midoritiba\
   belowsurface3000 teejayblack Andreydci Sempris ramazankarisan \
   AntonisZaravelas Spearsofcodes naomininnig noureddinalsabbagh; do
